59884f9b45030bf0435edcceb8f9eaa73a16f50d
Nicolas Britos <nicbritos@hotmail.com>
2023-03-01 23:43:55 -0300

    chore(backend): add Account interfaces

---
+++ backend/src/interfaces/repositories/AccountRepository.ts
@@ -0,0 +1,6 @@
+import { Account } from '@models/Account'
+import { ID, Nullable } from '@models/utils/UtilityTypes'
+
+export interface AccountRepository {
+  findAll(params?: Nullable<{ ids: ID[] }>): Promise<Account[]>;
+}
