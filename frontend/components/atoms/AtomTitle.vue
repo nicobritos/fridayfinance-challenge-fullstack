1dc6370e8d502b8fb52e28e632d2eb7b34fc588f
Nicolas Britos <nicbritos@hotmail.com>
2023-03-03 15:34:01 -0300

    feat(frontend): add AtomTitle

---
+++ frontend/components/atoms/AtomTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3 class="font-bold"><slot /></h3>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class AtomTitle extends Vue {}
+</script>
+
+<style scoped></style>
