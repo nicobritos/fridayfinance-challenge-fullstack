26498b7009d6e4b33d200644a1ff2959b1dc9097
Nicolas Britos <nicbritos@hotmail.com>
2023-03-03 15:34:15 -0300

    feat(frontend): add MoleculeTransactionFilter (rb me)

---
+++ frontend/components/molecules/transaction/MoleculeTransactionFilter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex items-end space-x-4">
+    <AtomTextInput
+      title="Search"
+      value=""
+      placeholder="Search by bank, account, reference, category, date, amount, currency..."
+      target-class="w-full"
+      class="flex-grow flex-shrink-0"
+    />
+    <AtomTextInput
+      title="Bank"
+      value=""
+      placeholder="No filter applied"
+      target-class="w-full"
+      class="flex-grow-0 flex-shrink max-w-8"
+    />
+    <AtomTextInput
+      title="Account"
+      value=""
+      placeholder="No filter applied"
+      target-class="w-full"
+      class="flex-grow-0 flex-shrink max-w-8"
+    />
+    <AtomTextInput
+      title="Starting month"
+      value=""
+      placeholder="No filter applied"
+      target-class="w-full"
+      class="flex-grow-0 flex-shrink max-w-8"
+    />
+    <AtomTextInput
+      title="Ending month"
+      value=""
+      placeholder="No filter applied"
+      target-class="w-full"
+      class="flex-grow-0 flex-shrink max-w-8"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AtomTextInput from '~/components/atoms/inputs/AtomTextInput.vue';
+import AtomInput from '~/components/atoms/inputs/AtomInput.vue';
+
+@Component({
+  components: { AtomInput, AtomTextInput },
+})
+export default class MoleculeTransactionFilter extends Vue {}
+</script>
+
+<style scoped></style>
