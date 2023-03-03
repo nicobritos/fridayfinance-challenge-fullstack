7e7afd6e76934e4c62dc6a87ee71847aa60b6fa4
Nicolas Britos <nicbritos@hotmail.com>
2023-03-03 15:33:52 -0300

    feat(frontend): add AtomTag

---
+++ frontend/components/atoms/AtomTag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex items-center justify-center py-1.5 px-2 rounded-md"
+    :style="colorStyle"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class AtomTag extends Vue {
+  @Prop({ type: String, required: true })
+  private declare readonly color: string;
+
+  get colorStyle(): string {
+    return `background-color: ${this.color};`;
+  }
+}
+</script>
+
+<style scoped></style>
