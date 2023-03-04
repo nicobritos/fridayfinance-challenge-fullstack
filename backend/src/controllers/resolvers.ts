e392dd3d2bcdab654d18e00bfbd804adfea085d7
Nicolas Britos <nicbritos@hotmail.com>
2023-03-04 18:34:53 -0300

    fix(backend): fix list accounts not working

--- backend/src/controllers/resolvers.ts
+++ backend/src/controllers/resolvers.ts
@@ -1,13 +1,16 @@
 import accountResolverImpl from './resolvers/AccountResolverImpl';
+import _ from 'lodash';
 
 import isoDateResolver from './resolvers/ISODate';
 import { Resolvers } from './resolvers/generated/generated';
 import transactionResolverImpl from '@controllers/resolvers/TransactionResolverImpl';
+import categoryResolverImpl from '@controllers/resolvers/CategoryResolverImpl';
 
-const resolvers: Resolvers = {
-  ...isoDateResolver,
-  ...accountResolverImpl,
-  ...transactionResolverImpl,
-};
+const resolvers: Resolvers = _.merge(
+  isoDateResolver,
+  accountResolverImpl,
+  categoryResolverImpl,
+  transactionResolverImpl
+);
 
 export default resolvers;
