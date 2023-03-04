1f1c2b3dd7e1dd627e08e79b040941a54172259d
Nicolas Britos <nicbritos@hotmail.com>
2023-03-04 18:14:50 -0300

    style(backend): lint with prettier

--- backend/prod-paths.js
+++ backend/prod-paths.js
@@ -1,10 +1,10 @@
-const tsConfig = require("./tsconfig.json");
-const tsConfigPaths = require("tsconfig-paths");
+const tsConfig = require('./tsconfig.json');
+const tsConfigPaths = require('tsconfig-paths');
 
-const baseUrl = "./dist"; // Either absolute or relative path. If relative it's resolved to current working directory.
+const baseUrl = './dist'; // Either absolute or relative path. If relative it's resolved to current working directory.
 const cleanup = tsConfigPaths.register({
-    baseUrl,
-    paths: tsConfig.compilerOptions.paths
+  baseUrl,
+  paths: tsConfig.compilerOptions.paths,
 });
 
 // When path registration is no longer needed
