37b144f806f7c400b4799ad8818f06ae7b032634
Nicolas Britos <nicbritos@hotmail.com>
2023-03-05 16:51:46 -0300

    style(frontend): run linter

--- frontend/components/molecules/MoleculeError.vue
+++ frontend/components/molecules/MoleculeError.vue
@@ -3,9 +3,9 @@
     <AtomTitle class="text-xl">An error has occurred:</AtomTitle>
     <AtomTitle class="text-lg">{{ error.statusCode }}</AtomTitle>
     <p>{{ error.message }}</p>
-    <div class='flex items-center justify-center'>
+    <div class="flex items-center justify-center">
       <AtomElevatedButton>
-        <NuxtLink to='/'>Back to home</NuxtLink>
+        <NuxtLink to="/">Back to home</NuxtLink>
       </AtomElevatedButton>
     </div>
   </AtomRoundedContainer>
@@ -16,7 +16,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import AtomRoundedContainer from '~/components/atoms/containers/AtomRoundedContainer.vue';
 import { NuxtError } from '@nuxt/types';
 import AtomTitle from '~/components/atoms/AtomTitle.vue';
-import AtomElevatedButton from '~/components/atoms/button/AtomElevatedButton.vue'
+import AtomElevatedButton from '~/components/atoms/button/AtomElevatedButton.vue';
 
 @Component({
   components: { AtomElevatedButton, AtomTitle, AtomRoundedContainer },
