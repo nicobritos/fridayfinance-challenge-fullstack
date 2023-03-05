4f9c90ab86ff3bc54b4c1ab7c5cc4d721be856b2
Nicolas Britos <nicbritos@hotmail.com>
2023-03-05 00:52:54 -0300

    feat(frontend): add back button

---
+++ frontend/components/molecules/MoleculeBackButtonTransactions.vue
@@ -0,0 +1,26 @@
+<template>
+  <AtomElevatedButton target-class="items-center space-x-1" @click="onClick">
+    <i class="fas fa-arrow-left"></i>
+    <span>Back to transactions</span>
+  </AtomElevatedButton>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AtomElevatedButton from '~/components/atoms/button/AtomElevatedButton.vue';
+
+@Component({
+  components: {
+    AtomElevatedButton,
+  },
+})
+export default class MoleculeBackButtonTransactions extends Vue {
+  public onClick(event?: Event): void {
+    event?.stopPropagation();
+    event?.preventDefault();
+    this.$router.push('/');
+  }
+}
+</script>
+
+<style scoped></style>
